1c4df01996450732bd3285027fbaa1a20ce386a8
Tong Lei <tonglei@qti.qualcomm.com>
2025-12-09 17:59:46 +0800

    complete ANI/A2NI function cmd/driver/halphy/log enable
    Signed-off-by: Tong Lei <tonglei@qti.qualcomm.com>
    Change-Id: I378d12de7aeb4e10497505ab801b9d51536f9e08

--- drivers/wifi/qcom/inc/qcom_wifi_mgmt.h
+++ drivers/wifi/qcom/inc/qcom_wifi_mgmt.h
@@ -6,18 +6,21 @@
 #ifndef QCOM_INCLUDE_NET_WIFI_MGMT_H_
 #define QCOM_INCLUDE_NET_WIFI_MGMT_H_
 
+#include <zephyr/net/wifi_mgmt.h>
+#include <stdint.h>
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/wifi.h>
 #include <zephyr/net/ethernet.h>
 #include <zephyr/net/offloaded_netdev.h>
-#include <zephyr/net/wifi_mgmt.h>
 
 /** @brief Qcom Wi-Fi management commands */
 enum qcom_net_request_wifi_cmd {
 	/** Set TX Power for Wi-Fi networks */
 	NET_REQUEST_WIFI_CMD_QCOM_SET_TX_POWER = 1,
 	/** Get TX Power for Wi-Fi networks */
-	NET_REQUEST_WIFI_CMD_QCOM_GET_TX_POWER
+	NET_REQUEST_WIFI_CMD_QCOM_GET_TX_POWER,
+	/** Unit test dispatch */
+	NET_REQUEST_WIFI_CMD_QCOM_UNIT_TEST,
 };
 
 /** Request a Wi-Fi set tx power */
@@ -31,6 +34,11 @@ NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_QCOM_SET_TX_POWER);
 	(_NET_WIFI_BASE | NET_REQUEST_WIFI_CMD_QCOM_GET_TX_POWER)
 
 NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_QCOM_GET_TX_POWER);
+
+#define NET_REQUEST_WIFI_QCOM_UNIT_TEST					\
+	(_NET_WIFI_BASE | NET_REQUEST_WIFI_CMD_QCOM_UNIT_TEST)
+
+NET_MGMT_DEFINE_REQUEST_HANDLER(NET_REQUEST_WIFI_QCOM_UNIT_TEST);
 
 /** Set TX Power parameters */
 struct qcom_wifi_set_tx_power_params{
@@ -50,6 +58,14 @@ struct qcom_wifi_get_tx_power_params{
     uint16_t target_power;
 	/** The power that is set to driver */
     uint16_t real_power;
+};
+
+/** Unit test dispatch params */
+struct qcom_wifi_unit_test_params {
+	uint8_t vdev_id;
+	uint8_t module_id;
+	uint16_t num_args;
+	uint32_t args[16];
 };
 
 /** Wi-Fi management API */
@@ -72,6 +88,16 @@ struct qcom_wifi_mgmt_ops {
 	 */
 	int (*get_tx_power)(const struct device *dev,
 		    struct qcom_wifi_get_tx_power_params *params);
+	/**
+	 * @brief Perform a unit test.
+	 *
+	 * @param dev Pointer to the device structure for the driver instance.
+	 * @param params Unit test parameters.
+	 *
+	 * @return 0 if ok, < 0 if error.
+	 */
+	int (*unit_test)(const struct device *dev,
+			struct qcom_wifi_unit_test_params *params);
 };
 
 #endif
