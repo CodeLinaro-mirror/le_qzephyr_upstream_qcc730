25acb4cc889062ff2d71281309f0b7bccce3d2b4
min hu <minhu@qti.qualcomm.com>
2026-02-13 11:58:39 +0800

    driver: fix the issue, In AP STA mode, when AP use WPA2, STA can not connect AP with open
    Change-Id: I0069fed40fc5f2993f49bf504d3b87fa0b372bc0

--- drivers/wifi/qcom/qcom_wifi_drv.c
+++ drivers/wifi/qcom/qcom_wifi_drv.c
@@ -481,6 +481,8 @@ static int qwifi_drv_connect(const struct device *dev, struct wifi_connect_req_p
                             sizeof(qapi_WLAN_Crypt_Type_e), false);
         qapi_WLAN_Set_Param(deviceId, __QAPI_WLAN_PARAM_GROUP_WIRELESS_SECURITY,
                             __QAPI_WLAN_PARAM_GROUP_SECURITY_PASSPHRASE, (void *)psk, psk_length, false);
+    } else {
+        wlan_clear_privacy();
     }
 
     qapi_WLAN_Commit(deviceId);
