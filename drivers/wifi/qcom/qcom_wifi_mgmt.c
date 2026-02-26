a6ac2514fb776fd2da04fee2467d6af750a1c302
xiaoxiang <xiaxia@qti.qualcomm.com>
2026-02-26 15:53:57 +0800

    PM: Adapt to Zephyr Wi-Fi PM related APIs
    Change-Id: I350e8dd799aca54238d085caffa606aabc952395

--- drivers/wifi/qcom/qcom_wifi_mgmt.c
+++ drivers/wifi/qcom/qcom_wifi_mgmt.c
@@ -673,4 +673,122 @@ static int wifi_set_device_id(uint64_t mgmt_request, struct net_if *iface,
 }
 NET_MGMT_REGISTER_REQUEST_HANDLER(NET_REQUEST_WIFI_QCOM_SET_DEVICE_ID, wifi_set_device_id);
 
+static int wifi_pm_bmps_enable(uint64_t mgmt_request, struct net_if *iface,
+				 void *data, size_t len)
+{
+	const struct device *dev = net_if_get_device(iface);
+	const struct qcom_wifi_mgmt_ops *qcom_wifi_mgmt_api = get_qcom_wifi_api(iface);
+	struct qcom_wifi_pm_bmps_params *params = data;
 
+	if (qcom_wifi_mgmt_api == NULL || qcom_wifi_mgmt_api->set_bmps_enable == NULL) {
+		return -ENOTSUP;
+	}
+
+	if (!net_if_is_admin_up(iface)) {
+		return -ENETDOWN;
+	}
+
+	if (!data || len != sizeof(*params)) {
+		return -EINVAL;
+	}
+
+	return qcom_wifi_mgmt_api->set_bmps_enable(dev, params);
+}
+
+NET_MGMT_REGISTER_REQUEST_HANDLER(NET_REQUEST_WIFI_PM_QCOM_SET_BMPS_ENABLE, wifi_pm_bmps_enable);
+
+static int wifi_pm_ignore_bc_mc_in_bmps(uint64_t mgmt_request, struct net_if *iface,
+				 void *data, size_t len)
+{
+	const struct device *dev = net_if_get_device(iface);
+	const struct qcom_wifi_mgmt_ops *qcom_wifi_mgmt_api = get_qcom_wifi_api(iface);
+	struct qcom_wifi_pm_ignore_bc_mc_params *params = data;
+
+	if (qcom_wifi_mgmt_api == NULL || qcom_wifi_mgmt_api->set_ignore_bc_mc_in_bmps == NULL) {
+		return -ENOTSUP;
+	}
+
+	if (!net_if_is_admin_up(iface)) {
+		return -ENETDOWN;
+	}
+
+	if (!data || len != sizeof(*params)) {
+		return -EINVAL;
+	}
+
+	return qcom_wifi_mgmt_api->set_ignore_bc_mc_in_bmps(dev, params);
+}
+
+NET_MGMT_REGISTER_REQUEST_HANDLER(NET_REQUEST_WIFI_PM_QCOM_IGNORE_BC_MC_IN_BMPS, wifi_pm_ignore_bc_mc_in_bmps);
+
+static int wifi_pm_set_power_optimization_enable_in_bmps(uint64_t mgmt_request, struct net_if *iface,
+				 void *data, size_t len)
+{
+	const struct device *dev = net_if_get_device(iface);
+	const struct qcom_wifi_mgmt_ops *qcom_wifi_mgmt_api = get_qcom_wifi_api(iface);
+	struct qcom_wifi_pm_power_optimization_params *params = data;
+
+	if (qcom_wifi_mgmt_api == NULL || qcom_wifi_mgmt_api->set_power_optimization_enable_in_bmps == NULL) {
+		return -ENOTSUP;
+	}
+
+	if (!net_if_is_admin_up(iface)) {
+		return -ENETDOWN;
+	}
+
+	if (!data || len != sizeof(*params)) {
+		return -EINVAL;
+	}
+
+	return qcom_wifi_mgmt_api->set_power_optimization_enable_in_bmps(dev, params);
+}
+
+NET_MGMT_REGISTER_REQUEST_HANDLER(NET_REQUEST_WIFI_PM_QCOM_SET_POWER_OPTIMIZATION_ENABLE_IN_BMPS, wifi_pm_set_power_optimization_enable_in_bmps);
+
+static int wifi_pm_set_compress_qos_null_enable_in_bmps(uint64_t mgmt_request, struct net_if *iface,
+				 void *data, size_t len)
+{
+	const struct device *dev = net_if_get_device(iface);
+	const struct qcom_wifi_mgmt_ops *qcom_wifi_mgmt_api = get_qcom_wifi_api(iface);
+	struct qcom_wifi_pm_compress_qos_null_params *params = data;
+
+	if (qcom_wifi_mgmt_api == NULL || qcom_wifi_mgmt_api->set_compress_qos_null_enable_in_bmps == NULL) {
+		return -ENOTSUP;
+	}
+
+	if (!net_if_is_admin_up(iface)) {
+		return -ENETDOWN;
+	}
+
+	if (!data || len != sizeof(*params)) {
+		return -EINVAL;
+	}
+
+	return qcom_wifi_mgmt_api->set_compress_qos_null_enable_in_bmps(dev, params);
+}
+
+NET_MGMT_REGISTER_REQUEST_HANDLER(NET_REQUEST_WIFI_PM_QCOM_SET_COMPRESS_QOS_NULL_ENABLE_IN_BMPS, wifi_pm_set_compress_qos_null_enable_in_bmps);
+
+static int wifi_pm_set_rx_filter_in_bmps(uint64_t mgmt_request, struct net_if *iface,
+				 void *data, size_t len)
+{
+	const struct device *dev = net_if_get_device(iface);
+	const struct qcom_wifi_mgmt_ops *qcom_wifi_mgmt_api = get_qcom_wifi_api(iface);
+	struct qcom_wifi_pm_rx_filter_params *params = data;
+
+	if (qcom_wifi_mgmt_api == NULL || qcom_wifi_mgmt_api->set_rx_filter_in_bmps == NULL) {
+		return -ENOTSUP;
+	}
+
+	if (!net_if_is_admin_up(iface)) {
+		return -ENETDOWN;
+	}
+
+	if (!data || len != sizeof(*params)) {
+		return -EINVAL;
+	}
+
+	return qcom_wifi_mgmt_api->set_rx_filter_in_bmps(dev, params);
+}
+
+NET_MGMT_REGISTER_REQUEST_HANDLER(NET_REQUEST_WIFI_PM_QCOM_SET_RX_FILTER_IN_BMPS, wifi_pm_set_rx_filter_in_bmps);
